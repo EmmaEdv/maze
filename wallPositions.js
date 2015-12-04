4078e06bdfa038dc8a3be91df366a21b25c3ecd6
Emma Edvardsson <emmed608@student.liu.se>
2015-12-04 14:45:45 +0100

    Walls

---
+++ wallPositions.js
@@ -0,0 +1,82 @@
+//Objekt med storlek och position
+var walls = [
+	//Lodräta
+	{
+		geo: {x: 5, y: 1, z: 0.5},
+		position: {x: 12.75, y: 0, z: 10}
+	},
+	{
+		geo: {x: 10, y: 1, z: 0.5},
+		position: {x: 0, y: 0, z: 10}
+	},
+	{
+		geo: {x: 5, y: 1, z: 0.5},
+		position: {x: -12.75, y: 0, z: 10}
+	},
+	{
+		geo: {x: 5, y: 1, z: 0.5},
+		position: {x: 2.5, y: 0, z: 5}
+	},
+
+	{
+		geo: {x: 10, y: 1, z: 0.5},
+		position: {x: 10.5, y: 0, z: 0}
+	},
+	{
+		geo: {x: 5, y: 1, z: 0.5},
+		position: {x: -2.5, y: 0, z: 0}
+	},
+
+	{
+		geo: {x: 5, y: 1, z: 0.5},
+		position: {x: -12.75, y: 0, z: 0}
+	},
+	{
+		geo: {x: 5, y: 1, z: 0.5},
+		position: {x: 8, y: 0, z: -5.25}
+	},
+
+
+	{
+		geo: {x: 5, y: 1, z: 0.5},
+		position: {x: 12.75, y: 0, z: -10.25}
+	},
+	{
+		geo: {x: 5, y: 1, z: 0.5},
+		position: {x: 2.5, y: 0, z: -10.25}
+	},
+	{
+		geo: {x: 5, y: 1, z: 0.5},
+		position: {x: -7.5, y: 0, z: -10.25}
+	},
+
+	//Vågräta
+	{
+		geo: {x: 0.5, y: 1, z: 5.5},
+		position: {x: 10.25, y: 0, z: 7.5}
+	},
+	{
+		geo: {x: 0.5, y: 1, z: 5.5},
+		position: {x: 5, y: 0, z: 7.5}
+	},
+	{
+		geo: {x: 0.5, y: 1, z: 15.5},
+		position: {x: -5, y: 0, z: 2.5}
+	},
+	{
+		geo: {x: 0.5, y: 1, z: 5.5},
+		position: {x: -10, y: 0, z: 7.5}
+	},
+	{
+		geo: {x: 0.5, y: 1, z: 10.75},
+		position: {x: 0, y: 0, z: -5.15}
+	},
+	{
+		geo: {x: 0.5, y: 1, z: 10.75},
+		position: {x: -10, y: 0, z: -5.15}
+	},
+	{
+		geo: {x: 0.5, y: 1, z: 5.5},
+		position: {x: 10.25, y: 0, z: -7.75}
+	}
+];
