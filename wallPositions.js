1cd2127d0f3320e20f8656ed29d631130d188b42
Emma Edvardsson <emmed608@student.liu.se>
2015-12-05 15:44:54 +0100

    Collision control done

--- wallPositions.js
+++ wallPositions.js
@@ -2,15 +2,15 @@
 var walls = [
 	//Lodräta
 	{
-		geo: {x: 5, y: 1, z: 0.5},
+		geo: {x: 5.5, y: 1, z: 0.5},
 		position: {x: 12.75, y: 0, z: 10}
 	},
 	{
-		geo: {x: 10, y: 1, z: 0.5},
+		geo: {x: 10.5, y: 1, z: 0.5},
 		position: {x: 0, y: 0, z: 10}
 	},
 	{
-		geo: {x: 5, y: 1, z: 0.5},
+		geo: {x: 5.5, y: 1, z: 0.5},
 		position: {x: -12.75, y: 0, z: 10}
 	},
 	{
@@ -23,30 +23,28 @@ var walls = [
 		position: {x: 10.5, y: 0, z: 0}
 	},
 	{
-		geo: {x: 5, y: 1, z: 0.5},
+		geo: {x: 5.5, y: 1, z: 0.5},
 		position: {x: -2.5, y: 0, z: 0}
 	},
 
 	{
-		geo: {x: 5, y: 1, z: 0.5},
+		geo: {x: 5.5, y: 1, z: 0.5},
 		position: {x: -12.75, y: 0, z: 0}
 	},
 	{
-		geo: {x: 5, y: 1, z: 0.5},
-		position: {x: 8, y: 0, z: -5.25}
+		geo: {x: 5.25, y: 1, z: 0.5},
+		position: {x: 7.25, y: 0, z: -5.25}
 	},
-
-
 	{
-		geo: {x: 5, y: 1, z: 0.5},
+		geo: {x: 5.5, y: 1, z: 0.5},
 		position: {x: 12.75, y: 0, z: -10.25}
 	},
 	{
-		geo: {x: 5, y: 1, z: 0.5},
+		geo: {x: 5.5, y: 1, z: 0.5},
 		position: {x: 2.5, y: 0, z: -10.25}
 	},
 	{
-		geo: {x: 5, y: 1, z: 0.5},
+		geo: {x: 5.5, y: 1, z: 0.5},
 		position: {x: -7.5, y: 0, z: -10.25}
 	},
 
